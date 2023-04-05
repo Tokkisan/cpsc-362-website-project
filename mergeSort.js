8a7fce0d5b046e9d539857877e0221081f5c5eb9
jason8452 <60160230+jason8452@users.noreply.github.com>
2023-04-05 16:55:29 -0700

    Update mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,67 +1,73 @@
-//merge sort algo
-function mergeSort(arr, steps = []) {
-  if (arr.length <= 1) {
-    return arr;
-  }
+function mergeSort(arr, steps) {
+//if array has 1 or empty, it's done
+        if (arr.length <= 1) {
+          return arr;
+        }
+//array into 2 halves
+        const middle = Math.floor(arr.length / 2);
+        const left = arr.slice(0, middle);
+        const right = arr.slice(middle);
 
-  const middle = Math.floor(arr.length / 2);
-  const left = arr.slice(0, middle);
-  const right = arr.slice(middle);
+//recursively sort left and right
+        const sortedLeft = mergeSort(left, steps);
+        const sortedRight = mergeSort(right, steps);
 
-  //calls it on left and right
-  const leftSorted = mergeSort(left, steps);
-  const rightSorted = mergeSort(right, steps);
+//merge sorted
+        const merged = merge(sortedLeft, sortedRight);
 
-  //merges the sorted
-  const merged = merge(leftSorted, rightSorted);
+//store step in visualizer
+        steps.push([...merged]);
 
-  //store the current step
-  steps.push([...merged]);
+//return merged array
+        return merged;
+      }
 
-  return merged;
-}
+      function merge(left, right) {
+      //initialize empty array
+        let result = [];
+        let i = 0;
+        let j = 0;
 
-function merge(left, right) {
-  let result = [];
-  let i = 0;
-  let j = 0;
+        while (i < left.length && j < right.length) {
+          if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+          } else {
+            result.push(right[j]);
+            j++;
+          }
+        }
 
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i]);
-      i++;
-    } else {
-      result.push(right[j]);
-      j++;
-    }
-  }
+        while (i < left.length) {
+          result.push(left[i]);
+          i++;
+        }
 
-  while (i < left.length) {
-    result.push(left[i]);
-    i++;
-  }
+        while (j < right.length) {
+          result.push(right[j]);
+          j++;
+        }
 
-  while (j < right.length) {
-    result.push(right[j]);
-    j++;
-  }
+        return result;
+      }
 
-  return result;
-}
+      function runMergeSort() {
+      //get input string and split into array
+        const inputStr = document.getElementById('input').value;
+        const inputArray = inputStr.split(',').map(Number);
 
-//starts sort and steps
-function startMergeSort() {
-  const inputArray = document.getElementById('input-array').value.split(',').map(Number);
-  const stepsContainer = document.getElementById('steps-container');
-  stepsContainer.innerHTML = '';
-
-  //calls mergesort with input array and empty steps
-  const sortedArray = mergeSort(inputArray, []);
-
-  //displays each step
-  sortedArray.forEach((step, index) => {
-    const stepElement = document.createElement('div');
-    stepElement.textContent = `Step ${index + 1}: [${step.join(', ')}]`;
-    stepsContainer.appendChild(stepElement);
-  });
-}
+			//container element to display steps
+        const stepsContainer = document.getElementById('steps-container');
+        stepsContainer.innerHTML = '';
+        
+        const steps = [];
+      //call mergesort func
+        const sortedArray = mergeSort(inputArray, steps);
+        
+			//display sorted array in steps
+        for (let i = 0; i < steps.length; i++) {
+          const stepElement = document.createElement('div');
+          stepElement.textContent = `Step ${i + 1}: [${steps[i].join(', ')}]`;
+          stepsContainer.appendChild(stepElement);
+        }
+      }
